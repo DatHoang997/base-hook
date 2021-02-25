75c8a9e33fac428806bb344f20ee981213dbf70d
dathoang997 <dathoang997@gmail.com>
2021-02-25 10:46:39 +0700

    done

--- src/module/page/claimSRM/index.js
+++ src/module/page/claimSRM/index.js
@@ -1,32 +1,35 @@
 import React, { useEffect, useState } from 'react'
 import StandardPage from '../StandardPage'
 import { Input, Row, Col, Button } from 'antd'
+import Service from '../../../service/service'
 import 'antd/dist/antd.css'
 import './style.scss'
 
 const stats = () => {
-  const [srmAddress, setSrmAddress] = useState('')
+  const [addressA, setAddressA] = useState('')
+  const service = new Service
 
-  const claimSRM = () => {
-
+  const find = async () => {
+    console.log('addressA',addressA)
+    let response = await service.find(addressA)
   }
-  console.log(srmAddress)
+
   return (
     <StandardPage>
        <Row>
         <Col span={4}></Col>
         <Col span={20}>
-          <p>SRM address:</p>
+          <p>Address:</p>
         </Col>
       </Row>
       <Row>
         <Col span={4}></Col>
         <Col span={10}>
-          <Input onChange={(e) => {setSrmAddress(e.target.value);}} />
+          <Input onChange={(e) => {setAddressA(e.target.value);}} />
         </Col>
         <Col span={1}></Col>
         <Col span={4}>
-          <Button type='primary' onClick={claimSRM}>Submit</Button>
+          <Button type='primary' onClick={find}>Submit</Button>
         </Col>
       </Row>
     </StandardPage>
